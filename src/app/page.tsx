d8a3ae6ecf16dc45621c37ffa637d40b093e46b2
Shashank Deep <48642522+consolenine@users.noreply.github.com>
2024-04-18 12:30:13 +0530

    Escaped Apostrophe On Hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
 				<section className="w-full">
 					<div className="container mx-auto mr-[20%] pt-64 flex justify-end">
 						<div className="w-full md:w-2/5 text-end">
-							<h4 className="text-4xl">SHASHANK DEEP'S</h4>
+							<h4 className="text-4xl">SHASHANK DEEP&apos;S</h4>
 							<h2 className="text-7xl text-poppins tracking-widest">Portfolio</h2>
 						</div>
 					</div>
